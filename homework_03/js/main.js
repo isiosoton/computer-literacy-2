093abcbd422e145596687013813844cca3b4c9dc
kameoka <e2058232@soka-u.jp>
2023-07-11 20:11:05 +0900

    comment形式の変更

--- homework_03/js/main.js
+++ homework_03/js/main.js
@@ -4,6 +4,6 @@ let comment = "";
 
 function main(player_hand) {
   const computer_hand = Math.floor(Math.random() * 3);
-  comment = "you:" + handtype[player_hand] + "\tcom:" + handtype[computer_hand] + "\t結果：あなたの" + resulttype[(player_hand - computer_hand + 3) % 3] + "\n" + comment;
+  comment = `you:${handtype[player_hand]}\tcom:${handtype[computer_hand]}\t結果：あなたの${resulttype[(player_hand - computer_hand + 3) % 3]}\n${comment}`;
   document.querySelector("#result").textContent = comment;
 }
