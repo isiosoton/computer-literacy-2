8a267aa19b748b90830691c2079c23db76a5b0f4
kameoka <e2058232@soka-u.jp>
2023-07-19 22:20:32 +0900

    バス停データアクセス時点

---
+++ homework_04/js/main.js
@@ -0,0 +1,12 @@
+function succession() {
+  // request_busstop_time();
+  const busstopdata = callApi();
+  console.log(busstopdata);
+  // downloadJson();
+
+  // envdata = require("../.env");
+  // console.log(envdata);
+  // const now = timeread();
+  // console.log(now);
+  // console.log(comment);
+}
---
+++ homework_04/js/read_busstop_time.js
@@ -0,0 +1,52 @@
+const soudai_busspot_list = ["創価大東京富士美術館（正門）", "創価大学栄光門"];
+
+async function callApi() {
+  // const API_Key = process.env.ODPT_ACCESS_TOKEN;
+  const API_Key = read_env();
+  //   console.log(API_Key);
+  //   バス時刻表
+  //   const res = await fetch(`https://api.odpt.org/api/v4/odpt:BusTimetable?odpt:operator=odpt.Operator:NishiTokyoBus&acl:consumerKey=${API_Key}`);
+  //   バス停情報
+  const res = await fetch(`https://api.odpt.org/api/v4/odpt:BusstopPole?odpt:operator=odpt.Operator:NishiTokyoBus&acl:consumerKey=${API_Key}`);
+  const users = await res.json();
+  const buspole_list = await buspole_data(users);
+  return buspole_list;
+}
+
+function buspole_data(json_data) {
+  buspole_list = [];
+  json_data.forEach((buspole, index) => {
+    if (buspole["dc:title"].match(/創価/)) {
+      buspole_list.push(buspole);
+    }
+  });
+  return buspole_list;
+}
+
+// jsonファイルをダウンロードする関数
+function downloadJson() {
+  const request = new XMLHttpRequest();
+  // request.open("GET", `https://api.odpt.org/api/v4/odpt:BusTimetable?odpt:operator=odpt.Operator:NishiTokyoBus&acl:consumerKey=${API_Key}`, true);
+  request.responseType = "blob";
+  request.onload = function () {
+    // レスポンスが帰ってきたら実行する処理
+    const blob = request.response;
+    const link = document.createElement("a");
+    // ダウンロードリンクの作成
+    link.href = window.URL.createObjectURL(blob);
+    link.download = "busstop.json";
+    // リンクをクリックさせる
+    link.click();
+  };
+  request.send();
+}
+
+// 現在の時刻を取得する関数
+function timeread() {
+  const now = new Date();
+  const hour = now.getHours();
+  const minute = now.getMinutes();
+  const second = now.getSeconds();
+  console.log(`${hour}:${minute}:${second}`);
+  return now;
+}
